825a63d592c76bfb475e737bec6a10ada187be15
Gaelan D'costa <gdcosta@gmail.com>
2016-11-17 22:59:04 -0500

    Add Leiningen project file

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject net.robot-disco/coffee-table "0.0.1"
+  :description "Review and log café visits"
+  :url "http://robot-disco.net/coffee-table"
+  :license {:name "GNU General Public License 3.0"
+            :url "http://www.gnu.org/licenses/gpl-3.0.txt"
+            :distribution :repo}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :source-paths ["src/clj" "src/cljc"]
+  :test-paths ["test/clj" "test/cljc"]
+  )
