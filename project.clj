67ce69acdf9c3b593d9775270ea8fc4685f924e3
Gaelan D'costa <gdcosta@gmail.com>
2016-12-27 21:37:25 -0500

    Figure out some Yada POST logic
    1) JSON-API and Yada have different opinions ... let's stick with
    Yada's for now. Yada is like Liberator and has some kind of flow,
    although it's not well-documented and one has to read the code.
    
    2) Given this I was able to successfully write some mock tests on HTTP
    responses for a POST (a.k.a. creating a new visit. Nothing that
    integrates back to the DB yet, but I am at the point where the
    gruntwork could happen.
    Some refactoring needs to happen soon though, need to extract the real
    meat of my resource out of the test. And where does it live?
    
    3) Interesting! Did you know that Yada can use Plumatic Schema objects
    to auto-validate input? Currently leveraging this for JSON request
    bodies.
    There's some implicit error handling where it looks like half-way
    decent messages get spit back to the user if something goes wrong from
    a client-side POV. Maybe I can let yada do the thinking for me.
    
    4) Removed the json_api.cljc I previously threw in because as I said
    Yada has its own opinions and I'll stick with them for now.
    
    Some little things:
    *) Yada only auto-returns 201 (Created) or 204 (Updated?) if the
     response body is nil. Yada knows which response code to use based on
     the [:properties :exists?] nested key-sequence inside the
     resource. Without the :properties key attributed to a function that
     returns the property key-values that should be inserted into whatever
     properties dictionary is computed as the default response, we seem to
     alays assume that this URI is dealing with some already-existing object.
    *) POST only returns a 201 (Created) if response body is a Java URI
     object.

--- project.clj
+++ project.clj
@@ -8,8 +8,9 @@
                  [prismatic/schema "1.1.3"]
                  [com.stuartsierra/component "0.3.1"]
                  [aleph "0.4.1"]
-                 [yada "1.1.44"]
-                 [ring/ring-mock "0.3.0"]]
+                 [yada "1.1.46"]
+                 [ring/ring-mock "0.3.0"]
+                 [cheshire "5.6.3"]]
   :profiles {:dev {:dependencies [[reloaded.repl "0.2.3"]]}
                    :source-paths ["dev"]}
   :source-paths ["src/clj" "src/cljc"]
--- test/clj/coffee_table/api_test.clj
+++ test/clj/coffee_table/api_test.clj
@@ -1,13 +1,40 @@
 (ns coffee-table.api-test
   (:require  [clojure.test :refer :all]
              [ring.mock.request :as mock]
-             [yada.yada :as yada]))
+             [yada.yada :as yada]
+             [coffee-table.model :refer [Visit]]
+             [cheshire.core :refer [generate-string]]
+             [byte-streams :as bs]))
+
+(defn make-json-request [request body]
+  (-> request
+      (mock/body (generate-string body))
+      (mock/header "Content-Type" "application/json")))
 
 (deftest visits-api
   (testing "POST /visits (invalid data)"
-    (let [handler (yada/handler "Totally fake handler")
-          response @(handler (mock/request :post "/visits"))]
-      (is (= 400 (:status response)))))
+    (let [resource {:methods {:post {:parameters {:body Visit}
+                                     :response (java.net.URI. "/visits/1")}}
+                    :consumes #{"application/json"}}
+          handler (yada/handler (yada/resource resource))
+          request (make-json-request (mock/request :post "/visits")
+                                     {})
+          response @(handler request)]
+      (is (= 400 (-> response :status)))))
+  (testing "POST /visits (valid data)"
+    (let [resource {:properties (fn [ctx] {:exists? false})
+                    :methods {:post {:parameters {:body Visit}
+                                     :response (java.net.URI. "/visits/1")}}
+                    :consumes #{"application/json"}}
+          handler (yada/handler (yada/resource resource))
+          request (make-json-request (mock/request :post "/visits")
+                                     {:name "Minumum Data"
+                                      :beverage-rating 5
+                                      :beverage-ordered "Cortado"
+                                      :date (java.util.Date.)})
+          response @(handler request)]
+      (is (= 201 (-> response :status)))
+      (is (contains? (:headers response) "location"))))
   #_ (testing "GET /visits"
     (let [handler (yada/handler "Totally fake handler")
           response @(handler (mock/request :get "/visits"))]
