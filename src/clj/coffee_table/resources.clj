636982bc415672151cab491227f5ba31a000d894
Gaelan D'costa <gdcosta@gmail.com>
2017-01-11 20:57:51 -0500

    Adding new record hooks into in-mem DB for reals
    Like we're actually populating the in-mem DB thus we're making a call
    to the DB component

--- src/clj/coffee_table/resources.clj
+++ src/clj/coffee_table/resources.clj
@@ -1,12 +1,16 @@
 (ns coffee-table.resources
   (:require [yada.yada :as yada]
-            [coffee-table.model :refer [Visit]])
+            [coffee-table.model :refer [Visit]]
+            [coffee-table.bullshit-database :as dbc])
   (:import [java.net URI]))
 
-(defn new-visit-resource []
+(defn new-visit-resource [db]
   (yada/resource
    {:description "Café Visit entries"
     :consumes #{"application/json"}
     :properties (fn [ctx] {:exists? false})
     :methods {:post {:parameters {:body Visit}
-                     :response (URI. "/visits/1")}}}))
+                     :response (fn [ctx]
+                                 (let [visit (dbc/add-visit db (get-in ctx [:parameters :body]))
+                                       id (:id visit)]
+                                   (URI. (str "/visits/" id))))}}}))
