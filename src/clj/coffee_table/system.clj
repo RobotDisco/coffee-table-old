a702f12ebcb52fa84df88ef69b1971e8627bf256
Gaelan D'costa <gdcosta@gmail.com>
2017-02-20 03:33:56 -0500

    use postgres DB component not fake DB

--- src/clj/coffee_table/system.clj
+++ src/clj/coffee_table/system.clj
@@ -1,18 +1,19 @@
 (ns coffee-table.system
   (:require [com.stuartsierra.component :as component]
+            [coffee-table.database :as dbc]
             [coffee-table.bullshit-database :as bsd]
             [coffee-table.web-server :as ws]))
 
 (defn dev-system []
   (component/system-map
-   :db (bsd/new-bullshit-database)
+   :db (dbc/new-database)
    :web (component/using
          (ws/new-web-server)
          [:db])))
 
 (defn test-system []
   (component/system-map
-   :db (bsd/new-bullshit-database)
+   :db (dbc/new-database)
    :web (component/using
          (ws/new-mock-web-server)
          [:db])))
