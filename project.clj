b1ee68982672ad01a712ab3cdffb7e5a4d89b3a9
Gaelan D'costa <gdcosta@gmail.com>
2017-08-21 00:13:31 -0400

    Fix React version mismatches via exclusions

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :local-repo ".m2"
   :dependencies [;; Core Language(s)
                  [org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.854"]
+                 [org.clojure/clojurescript "1.9.908"]
 
                  ;; Model schema/verification
                  [prismatic/schema "1.1.6"]
@@ -19,37 +19,33 @@
                  [aleph "0.4.3"]
 
                  ;; API: REST framework
-                 [yada "1.2.6"]
+                 [yada "1.2.8"]
 
                  ;; JSON parsing
-                 [cheshire "5.7.1"]
+                 [cheshire "5.8.0"]
 
                  ;; DB: SQL, migrations
                  [com.layerware/hugsql "0.4.7"]
                  [org.postgresql/postgresql "42.1.4"]
-                 [migratus "0.9.8"]
+                 [migratus "0.9.9"]
                  [org.slf4j/slf4j-log4j12 "1.7.25"]
 
-                 ;; Semantic UI (CSS Framework)
-                 [cljsjs/semantic-ui-react "0.71.0-0"]
+                 ;; Reagent/Re-frame state inspector
+                 [re-frisk "0.4.5"]
 
                  ;; Re-frame (Redux for Clojurescript)
-                 [re-frame "0.9.4"]
+                 [re-frame "0.10.1"]
                  ;; Re-frame AJAX effect handlers
                  [cljs-ajax "0.6.0"]
                  [day8.re-frame/http-fx "0.1.4"]
 
-                 ;; Reagent (React Framework)
-                 [reagent "0.7.0"]
-
-                 ;; Reagent/Re-frame state inspector
-                 [re-frisk "0.4.5"]
+                 ;; Semantic UI (CSS Framework)
+                 [cljsjs/semantic-ui-react "0.71.0-0" :exclusions [cljsjs/react cljsjs/react-dom]]
 
                  ;; Time Processing libraries
                  [com.andrewmcveigh/cljs-time "0.5.1"]]
   :plugins [[lein-figwheel "0.5.9"]
             [lein-cljsbuild "1.1.5"]
-            [cider/cider-nrepl "0.14.0"]
             [refactor-nrepl "2.2.0"]
             [migratus-lein "0.5.0"]]
   :profiles {:dev {:dependencies [;; Component/namespace mgmt
@@ -60,7 +56,7 @@
                                   [devcards "0.2.3" :exclusions [cljsjs/react cljsjs/react-dom]]
                                   ;; CLJS REPL
                                   [com.cemerick/piggieback "0.2.2"]
-                                  [figwheel-sidecar "0.5.12"]
+                                  [figwheel-sidecar "0.5.13"]
                                   ;; Chrome DevTools extensions for CLJS
                                   [binaryage/devtools "0.9.4"]]
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
